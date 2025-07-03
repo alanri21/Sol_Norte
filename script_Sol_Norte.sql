46ef116b7d6b0f81edb50c4047f624a04b20064f
Alan Adriel Riquelme <alriquelme@alumno.unlam.edu.ar>
2025-07-03 11:58:31 -0300

    Se crea SPs para la tabla socio y categoria_socio

--- script_Sol_Norte.sql
+++ script_Sol_Norte.sql
@@ -89,5 +89,154 @@ IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'socio')
 	GO
 
 	
+----------------------------------------------------
+-- CREACION DE STORED PROCEDURE
+----------------------------------------------------
 
+--TABLA socios.categoria_socio
+--INSERT
+CREATE OR ALTER PROCEDURE insert_categoria_socio
+	@par_nombreCategoria VARCHAR(20),
+    @par_costoMembresia  DECIMAL(18,2)
+AS
+BEGIN
+    INSERT INTO socios.categoria_socio (nombre_categoria, costo_membresia)
+    VALUES (@par_nombreCategoria, @par_costoMembresia);
+END;
+GO
 
+--UPDATE
+
+CREATE OR ALTER PROCEDURE update_categoria_socio
+	@par_categoriaSocioID INT,
+    @par_nombreCategoria  VARCHAR(20),
+    @par_costoMembresia   DECIMAL(18,2)
+AS
+BEGIN
+
+UPDATE socios.categoria_socio
+SET nombre_categoria = @par_nombreCategoria,
+    costo_membresia  = @par_costoMembresia
+WHERE categoria_socio_id = @par_categoriaSocioID;
+
+END;
+GO
+
+--DELETE
+CREATE OR ALTER PROCEDURE  socios.delete_CategoriaSocio
+    @par_categoriaSocioID INT
+AS
+BEGIN
+DELETE FROM socios.categoria_socio
+WHERE categoria_socio_id = @par_categoriaSocioID;
+
+END;
+GO
+
+-- TABLA socios.socio
+-- INSERT
+CREATE OR ALTER PROCEDURE insert_socio
+    @par_categoriaSocioID     INT,
+    @par_nombre               VARCHAR(50),
+    @par_apellido             VARCHAR(50),
+    @par_edad                 TINYINT,
+    @par_fec_inscripcion      DATE,
+    @par_dni                  CHAR(8),
+    @par_email                VARCHAR(50),
+    @par_fec_nacimiento       DATE,
+    @par_tel_contacto         VARCHAR(20),
+    @par_tel_contacto_emergencia VARCHAR(20),
+    @par_nombre_prepaga       VARCHAR(30),
+    @par_nro_socio_prepaga    VARCHAR(20),
+    @par_tel_prepaga_emergencia VARCHAR(20),
+    @par_responsable_pago     INT,
+    @par_parentezco           VARCHAR(15)
+AS
+BEGIN
+    INSERT INTO socios.socio (
+        categoria_socio_id,
+		nombre,
+		apellido,
+		edad,
+        fec_inscripcion,
+		dni,
+		email,
+		fec_nacimiento,
+        tel_contacto,
+		tel_contacto_emergencia,
+        nombre_prepaga,
+		nro_socio_prepaga,
+		tel_prepaga_emergencia,
+        responsable_pago,
+		parentezco
+    )
+    VALUES (
+        @par_categoriaSocioID,
+		@par_nombre,
+		@par_apellido,
+		@par_edad,
+        @par_fec_inscripcion,
+		@par_dni,
+		@par_email,
+		@par_fec_nacimiento,
+        @par_tel_contacto,
+		@par_tel_contacto_emergencia,
+        @par_nombre_prepaga,
+		@par_nro_socio_prepaga,
+		@par_tel_prepaga_emergencia,
+        @par_responsable_pago,
+		@par_parentezco
+    );
+END;
+GO
+
+-- UPDATE
+CREATE OR ALTER PROCEDURE update_socio
+    @par_nro_socio             INT,
+    @par_categoriaSocioID      INT,
+    @par_nombre               VARCHAR(50),
+    @par_apellido             VARCHAR(50),
+    @par_edad                 TINYINT,
+    @par_fec_inscripcion      DATE,
+    @par_dni                  CHAR(8),
+    @par_email                VARCHAR(50),
+    @par_fec_nacimiento       DATE,
+    @par_tel_contacto         VARCHAR(20),
+    @par_tel_contacto_emergencia VARCHAR(20),
+    @par_nombre_prepaga       VARCHAR(30),
+    @par_nro_socio_prepaga    VARCHAR(20),
+    @par_tel_prepaga_emergencia VARCHAR(20),
+    @par_responsable_pago     INT,
+    @par_parentezco           VARCHAR(15)
+AS
+BEGIN
+    UPDATE socios.socio
+    SET 
+        categoria_socio_id         = @par_categoriaSocioID,
+        nombre                     = @par_nombre,
+        apellido                   = @par_apellido,
+        edad                       = @par_edad,
+        fec_inscripcion            = @par_fec_inscripcion,
+        dni                        = @par_dni,
+        email                      = @par_email,
+        fec_nacimiento             = @par_fec_nacimiento,
+        tel_contacto               = @par_tel_contacto,
+        tel_contacto_emergencia    = @par_tel_contacto_emergencia,
+        nombre_prepaga             = @par_nombre_prepaga,
+        nro_socio_prepaga          = @par_nro_socio_prepaga,
+        tel_prepaga_emergencia     = @par_tel_prepaga_emergencia,
+        responsable_pago           = @par_responsable_pago,
+        parentezco                 = @par_parentezco
+    WHERE nro_socio = @par_nro_socio;
+END;
+GO
+
+-- DELETE
+CREATE OR ALTER PROCEDURE delete_socio
+    @par_nro_socio INT
+AS
+BEGIN
+    DELETE FROM socios.socio
+    WHERE nro_socio = @par_nro_socio;
+END;
+GO
